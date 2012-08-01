62eec23fa47efedfdc1af559db6c20250b231645
James Long <jlongster@jlongster.com>
2012-08-01 15:28:41 -0400

    new build command

---
+++ default-project/tools/build.js
@@ -0,0 +1,13 @@
+({ 
+    appDir: "../www",
+    baseUrl: "js/lib",
+    dir: "../www-built",
+    paths: {
+        "app": "../app"
+    },
+    modules: [
+        { name: "app" }
+    ]
+})
+    
+        
